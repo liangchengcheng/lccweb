df8c5045c4c99b70f9d026734f1fdb2dc2aa51ad
liangchengcheng <1038127753@qq.com>
2016-08-18 22:25:26 +0800

    工具类
    工具类

--- src/main/java/com/hdsx/exception/GlobalExceptionResolver.java
+++ src/main/java/com/hdsx/exception/GlobalExceptionResolver.java
@@ -1,6 +1,7 @@
 package com.hdsx.exception;
 
 import com.alibaba.fastjson.JSON;
+import com.hdsx.dto.BaseResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,24 +16,34 @@ import java.io.PrintWriter;
 
 /**
  * 错误信息的统一处理
+ * 
  * @author lcc
  *
  */
 @Component
-public class GlobalExceptionResolver implements HandlerExceptionResolver{
+public class GlobalExceptionResolver implements HandlerExceptionResolver {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
-	
+
 	@ResponseBody
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception ex) {
-		//在这里其实有2种选择
-		//跳转到定制化的错误页面或者返回json信息
-		//ModelAndView error= new ModelAndView("error");
-		//error.addObject("exMsg",ex.getMessage());
-		//error.addObject("exType",ex.getClass().getSimpleName().replace("\"", "'"));
-		
+		LOG.error("访问" + request.getRequestURI() + " 发生错误, 错误信息:" + ex.getMessage());
+		// 在这里其实有2种选择
+		// 跳转到定制化的错误页面或者返回json信息
+		// ModelAndView error= new ModelAndView("error");
+		// error.addObject("exMsg",ex.getMessage());
+		// error.addObject("exType",ex.getClass().getSimpleName().replace("\"",
+		// "'"));
+		try {
+			PrintWriter writer = response.getWriter();
+			BaseResult<String> result = new BaseResult<>(false, ex.getMessage());
+			writer.write(JSON.toJSONString(result));
+			writer.flush();
+		} catch (Exception e) {
+			LOG.error("Exception:", e);
+		}
 		return null;
 	}
-	
+
 }
